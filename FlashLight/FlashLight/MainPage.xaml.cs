4ba44b7eda48924736e7469862333c21f76b3809
Blerim Sherifi <blerim_sherifi@live.com>
2018-05-31 01:09:47 +0200

    FlashLight Application - Android Version Tested

---
+++ FlashLight/FlashLight/MainPage.xaml.cs
@@ -0,0 +1,50 @@
+﻿using System;
+using Xamarin.Essentials;
+using Xamarin.Forms;
+
+namespace FlashLight
+{
+    public partial class MainPage : ContentPage
+    {
+        private bool TurnOn = true;
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void btnTurnLight_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                if (TurnOn)
+                {
+                    TurnOn = false;
+                    btnTurnLight.Image = "OFF.png";
+                    // Turn On
+                    await Flashlight.TurnOnAsync();
+                    Vibration.Vibrate();
+                }
+                else
+                {
+                    TurnOn = true;
+                    btnTurnLight.Image = "ON.png";
+                    // Turn Off
+                    await Flashlight.TurnOffAsync();
+                    Vibration.Vibrate();
+                }
+            }
+            catch (FeatureNotSupportedException fnsEx)
+            {
+                // Handle not supported on device exception
+            }
+            catch (PermissionException pEx)
+            {
+                // Handle permission exception
+            }
+            catch (Exception ex)
+            {
+                // Unable to turn on/off flashlight
+            }
+        }
+    }
+}
